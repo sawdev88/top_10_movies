f2a47a464c7951e8e5f27d463163cdb166f055db
Sean Wines <seanwinesdev@gmail.com>
2016-08-09 16:29:18 -0700

    Refactored js

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-let baseKey = 'ae86fc8dd2dce3fdad05baa89a0f32e4',
-    baseIMG = 'http://image.tmdb.org/t/p/w300';
+function baseKey() {
+    var key = 'ae86fc8dd2dce3fdad05baa89a0f32e4';
+    return key;
+}
 
-$.getJSON( 'https://api.themoviedb.org/3/discover/movie?api_key=' + baseKey + '&sort_by=popularity.desc', function(json) {
+$.getJSON('https://api.themoviedb.org/3/discover/movie?api_key=' + baseKey() + '&sort_by=popularity.desc', function(json) {
 
-  console.log( json );
+    let posters = $('li img'),
+        movieDetails = $('.movie-details'),
+        mainPoster = $('.main-poster'),
+        title = $('.title'),
+        overview = $('.overview'),
+        rating = $('.rating'),
+        baseIMG = 'http://image.tmdb.org/t/p/w300';
 
-  var posters = $('li img'),
-      movieDetails = $('.movie-details'),
-      mainPoster = $('.main-poster'),
-      title = $('.title'),
-      overview = $('.overview'),
-      rating = $('.rating');
+    posters.each(function(index) {
+        console.log(index);
+        $(this).attr('src', baseIMG + json.results[index].poster_path);
 
-  posters.each(function (index) {
-    console.log(index);
-    $(this).attr('src', baseIMG + json.results[index].poster_path);
-
-    $(this).on('click', function () {
-      movieDetails.fadeIn();
-      mainPoster.attr('src', baseIMG + json.results[index].poster_path);
-      title.text(json.results[index].title);
-      overview.text(json.results[index].overview);
-      rating.text(json.results[index].vote_average + '/10');
+        $(this).on('click', function() {
+            movieDetails.fadeIn();
+            mainPoster.attr('src', baseIMG + json.results[index].poster_path);
+            title.text(json.results[index].title);
+            overview.text(json.results[index].overview);
+            rating.text(json.results[index].vote_average + '/10');
+        })
     })
-  })
 
-  movieDetails.on('click', function () {
-    $(this).fadeOut();
-  })
+    movieDetails.on('click', function() {
+        $(this).fadeOut();
+    })
 
 });
